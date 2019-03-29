8c2886ffa4bd7ef8301cc7cb3922927f936700f0
Marina Baskova <marinabaskova@MacBook-Pro-11524.local>
2019-03-29 10:11:06 -0700

    changed header  component into styled component

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -3,14 +3,57 @@ import styled, { css } from 'styled-components';
 
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
+const HeaderWrap = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: row;
+background-color: #fff;
+border-bottom: 1px solid lightgrey;
+width: 100%;
+height: 120px;
+margin-top: 44px;
+padding-bottom: 15px;
+}
+@media (min-width: 1280px) {
+  width: 1280px;
+}
+`;
+
+const Date = styled.span`
+	align-self: flex-end;
+	font-size: 11px;
+	font-weight: bold;
+	letter-spacing: 1px;
+	margin-left: 25px;
+	flex: 1;
+`;
+
+const Temp = styled.span`
+	align-self: flex-end;
+	font-size: 11px;
+	font-weight: bold;
+	letter-spacing: 1px;
+	text-align: right;
+	margin-right: 25px;
+	flex: 1;
+`;
+const HeaderTitle = styled.div`
+	font-size: 60px;
+	font-family: Didot, serif;
+	font-weight: bold;
+	flex: 8;
+	text-align: center;
+	color: #000;
+`;
 
 const Header = () => {
 	return (
-		<div className="header">
-			<span className="date">SMARCH 32, 2018</span>
-			<h1>Lambda Times</h1>
-			<span className="temp">98°</span>
-		</div>
+		<HeaderWrap>
+			<Date>SMARCH 32, 2018</Date>
+			<HeaderTitle>Lambda Times</HeaderTitle>
+			<Temp>98°</Temp>
+		</HeaderWrap>
 	);
 };
 
