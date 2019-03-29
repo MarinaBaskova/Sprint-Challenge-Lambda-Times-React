73656925211c1b201db20410e634a2929345e8e5
Marina Baskova <marinabaskova@MacBook-Pro-11524.local>
2019-03-29 10:42:07 -0700

    added answers for readme

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -3,21 +3,22 @@ import styled, { css } from 'styled-components';
 
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
+
 const HeaderWrap = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-direction: row;
-background-color: #fff;
-border-bottom: 1px solid lightgrey;
-width: 100%;
-height: 120px;
-margin-top: 44px;
-padding-bottom: 15px;
-}
-@media (min-width: 1280px) {
-  width: 1280px;
-}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  }
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
 `;
 
 const Date = styled.span`
